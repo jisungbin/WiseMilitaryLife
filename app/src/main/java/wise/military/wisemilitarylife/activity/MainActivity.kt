886c5d5b6dfe0737cf31c083946edb4a28e9d540
jisungbin <ji@sungb.in>
2021-09-07 17:38:39 +0900

    Feat: 메인 화면 UI 완성

--- app/src/main/java/wise/military/wisemilitarylife/activity/MainActivity.kt
+++ app/src/main/java/wise/military/wisemilitarylife/activity/MainActivity.kt
@@ -9,30 +9,38 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import wise.military.wisemilitarylife.R
 import wise.military.wisemilitarylife.theme.MaterialTheme
 import wise.military.wisemilitarylife.theme.SystemUiController
-import wise.military.wisemilitarylife.theme.colors
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        SystemUiController(window).apply {
-            setStatusBarColor(colors.primary)
-            setNavigationBarColor(Color.White)
-        }
+        SystemUiController(window).setSystemBarsColor(Color.White)
         setContent {
             MaterialTheme {
                 Content()
@@ -42,15 +50,29 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     private fun Content() {
+        var idField by remember { mutableStateOf(TextFieldValue()) }
+        var passwordField by remember { mutableStateOf(TextFieldValue()) }
+        val outlineTextFieldBorderTheme = TextFieldDefaults.outlinedTextFieldColors(
+            textColor = Color.Black,
+            disabledTextColor = Color.Gray,
+            backgroundColor = Color.White,
+            cursorColor = Color.Black,
+            focusedBorderColor = Color.Gray,
+            unfocusedBorderColor = Color.LightGray,
+            disabledBorderColor = Color.LightGray,
+        )
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(30.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceAround
         ) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .wrapContentWidth()
+                    .wrapContentWidth(),
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Image(
                     painter = painterResource(R.mipmap.ic_launcher),
@@ -60,10 +82,43 @@ class MainActivity : ComponentActivity() {
                 Text(
                     text = stringResource(R.string.app_name),
                     color = Color.Black,
-                    modifier = Modifier.padding(start = 16.dp)
+                    modifier = Modifier.padding(start = 16.dp),
+                    fontSize = 20.sp
                 )
             }
-            Column() {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(15.dp)
+            ) {
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = idField,
+                    onValueChange = { idField = it },
+                    colors = outlineTextFieldBorderTheme,
+                    placeholder = { Text(text = "아이디") },
+                    singleLine = true
+                )
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = passwordField,
+                    onValueChange = { passwordField = it },
+                    colors = outlineTextFieldBorderTheme,
+                    visualTransformation = PasswordVisualTransformation(),
+                    placeholder = { Text(text = "비밀번호") },
+                    singleLine = true
+                )
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp),
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text(text = "로그인")
+                }
+                Text(text = "회원가입", color = Color.Gray)
             }
         }
     }
